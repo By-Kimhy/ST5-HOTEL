d6acad957634da5cb8010a419386770bb286a85e
kimhy0325 <kimhyby99@gmail.com>
2025-02-26 19:59:23 +0700

    Add Teacher Management

--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -18,8 +18,9 @@
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard v1</li>
+                        <li class="breadcrumb-item"><a href="#">Teaher Management</a></li>
+                        <li class="breadcrumb-item active">Teacher</li>
+                        <li class="breadcrumb-item active">Teacher Details</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
